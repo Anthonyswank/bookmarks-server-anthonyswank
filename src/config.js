4461427ab2ef0bd6f3b1ef6d9125be6fafa094d4
Anthony Swank <anthonyswank1999@gmail.com>
2021-01-19 00:37:53 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    API_TOKEN: process.env.API_TOKEN || 'c890dc5e-8d47-4979-a849-9e25962fea23',
+  }
+  
